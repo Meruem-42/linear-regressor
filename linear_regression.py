936bfef4ea400b4bad49ae5802850a56def46c4d
Alexandre Herlaud <aherlaud@student.42.fr>
2026-03-17 12:02:18 +0100

    new feature (main) : add the denormalization formula for the coefficient so they are standardized and ready to predict directly from the mileage input. Need to clean and start bonuses

--- linear_regression.py
+++ linear_regression.py
@@ -13,7 +13,27 @@ def normalization(feature_list) :
     norm_list = (feature_list - min)/(max - min)
     return (norm_list)
 
-def linear_regression(df) :
+def persistent_coeff(theta0, theta1) :
+    with open("thetas.json", "r") as f :
+        data = json.load(f)
+    data["theta0"] = theta0
+    data["theta1"] = theta1
+    with open("thetas.json", "w") as f :
+        json.dump(data, f, indent=4)
+    print("TEST MY LINEAR REGRESSION: ", (theta0, theta1))
+
+def denormalization(df, theta0, theta1) :
+    km_min = df["km"].min()
+    km_max = df["km"].max()
+    price_min = df["price"].min()
+    price_max = df["price"].max()
+    km_range = km_max - km_min
+    price_range = price_max - price_min
+    real_theta1 = theta1 * (price_range / km_range)
+    real_theta0 = theta0 * price_range + price_min - real_theta1 * km_min
+    persistent_coeff(real_theta0, real_theta1)
+
+def linear_regression_test(df) :
     X = df[['km']]
     y = df['price']
 
@@ -30,25 +50,20 @@ def main() :
     iterations = 10000
     with open("data.csv", "r") as f :
         data = pd.read_csv(f)
-    data["km"] = normalization(data["km"])
-    # data["price"] = normalization(data["price"])
+    km_norm = normalization(data["km"])
+    price_norm = normalization(data["price"])
     for j in range(iterations) :
-        gradient0 = np.array([theta0 + theta1*data["km"][i] - data["price"][i] for i in range(len(data["price"]))]).mean()
-        gradient1 = np.array([(theta0 + theta1*data["km"][i] - data["price"][i])*data["km"][i] for i in range(len(data["price"]))]).mean()
+        predictor = theta0 + theta1*km_norm
+        error = predictor - price_norm
+
+        gradient0 = error.mean()
+        gradient1 = (error*km_norm).mean()
+
         theta0 -= step*gradient0
         theta1 -= step*gradient1
-    mse_list = [pow(theta0 + theta1*data["km"][i] - data["price"][i], 2) for i in range(len(data["price"]))]    
-    mse_result = np.array(mse_list).sum()/(len(data["price"])*2)
-    linear_regression(data)
-    with open("thetas.json", "r") as f :
-        data = json.load(f)
-    data["theta0"] = theta0
-    data["theta1"] = theta1
-    with open("thetas.json", "w") as f :
-        json.dump(data, f, indent=4)
 
-    print("TEST MY LINEAR REGRESSION: ", (theta0, theta1))
-    print(mse_result)
+    linear_regression_test(data)
+    denormalization(data, theta0, theta1)
 
 
 if __name__ == "__main__" :
